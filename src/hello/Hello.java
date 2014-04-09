1b5ba9fad5e2d43845de86370f0ac5908e6a94e0
tcs02 <>
2014-04-09 16:47:42 +0900

    println added.

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -14,6 +14,7 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello, world");
         // TODO code application logic here
     }
 }
